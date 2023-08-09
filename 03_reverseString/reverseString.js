3e1e4402d46533fc002f2f0dcaf0269ef5b6b46b
LyricChan <71299332+LyricChan@users.noreply.github.com>
2023-08-09 18:41:59 -0400

    Reverse a string and pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  //   let splitWord = word.split("");
+  //   let reverseWord = splitWord.reverse();
+  //   let finalString = reverseWord.join("");
 
+  let finalString = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    finalString += word[i];
+  }
+
+  return finalString;
 };
 
 // Do not edit below this line
