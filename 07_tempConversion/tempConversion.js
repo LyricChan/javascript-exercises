ad1f7b41e7d0913817491f198f1422d5aea09c90
LyricChan <71299332+LyricChan@users.noreply.github.com>
2023-08-11 14:01:16 -0400

    Convert fahrenheit to celcsius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farh) {
+  return Math.round((farh - 32) * (5 / 9)) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celc) {
+  return Math.round(celc * (9 / 5) + 32) / 10;
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
