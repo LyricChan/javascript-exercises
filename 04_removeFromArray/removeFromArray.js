c0a0e08b93a6d93dfdb98e26461ee94c00aae8b4
LyricChan <71299332+LyricChan@users.noreply.github.com>
2023-08-11 13:49:57 -0400

    Finish JS exercises and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  //   let newArr = [];
 
+  // args.sort(function (a, b) {
+  //   return a - b;
+  // });
+
+  // for (const arg of args) {
+  //   for (const num of arr) {
+  //     if (num === arg) {
+  //       arr.splice(arg - 1, 1);
+  //     }
+  //   }
+  //   return arr;
+  // }
+
+  const newArr = [];
+
+  arr.forEach((items) => {
+    if (!args.includes(items)) {
+      newArr.push(items);
+    }
+  });
+
+  return newArr;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  //leap year neneds to be divisible by 4
+  // leap years divisible by 100 are not leap years
+  // unless divisible by 400
+  let yearDividedby4 = year % 4 == 0 ? true : false;
+  let centuryYear = !(year % 100 == 0) || year % 400 == 0 ? true : false;
 
+  //   if (yearDividedby4 && !(year % 100 == 0) && year % 400 == 0) {
+  //     return true;
+  //   } else return false;
+  if (yearDividedby4 && centuryYear) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
